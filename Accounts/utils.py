34e26dc9e7b5f1d43aac62848f4a5b7201f64cbf
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2023-07-27 14:45:27 +0330

    Change CustomIran Date and DateTime Fields

--- Accounts/utils.py
+++ Accounts/utils.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib import messages
 from django.shortcuts import redirect
+import pytz
+import datetime
+import jdatetime
 
 
 class IranPhoneNumberValidator(RegexValidator):
@@ -18,23 +21,52 @@ class ShamsiDateField(models.DateField):
         """
         if value is None:
             return value
-        import jdatetime
-        if value.year < 1400:
-            return jdatetime.date(year=value.year, month=value.month, day=value.day)
-        return jdatetime.date.fromgregorian(year=value.year, month=value.month, day=value.day)
+        elif hasattr(value, "astimezone"):
+            value = value.astimezone(pytz.timezone("Asia/Tehran"))
+            return jdatetime.date.fromgregorian(year=value.year, month=value.month, day=value.day)
+        return value
 
     def to_python(self, value):
-        import jdatetime
-        import datetime
-        if isinstance(value, datetime.date):
-            return jdatetime.date(year=value.year, month=value.month, day=value.day)
-        return None
+        if value is None or isinstance(value, datetime.datetime):
+            return value
+        elif hasattr(value, "astimezone"):
+            return value.astimezone(pytz.utc)
+        return value
 
     def get_prep_value(self, value):
         if value is None:
             return None
-        import jdatetime
-        return jdatetime.date(value.year, value.month, value.day)
+        elif hasattr(value, "astimezone"):
+            return value.astimezone(pytz.utc)
+        return value
+
+
+class ShamsiDateTimeField(models.DateTimeField):
+    def from_db_value(self, value, expression, connection):
+        """
+        this method is responsible for converting the date stored in the database to the corresponding Shamsi date.
+        """
+        if value is None:
+            return value
+        elif hasattr(value, "astimezone"):
+            value = value.astimezone(pytz.timezone("Asia/Tehran"))
+            return jdatetime.datetime.fromgregorian(year=value.year, month=value.month, day=value.day, hour=value.hour,
+                                                    minute=value.minute, second=value.second)
+        return value
+
+    def to_python(self, value):
+        if value is None or isinstance(value, datetime.datetime):
+            return value
+        elif hasattr(value, "astimezone"):
+            return value.astimezone(pytz.utc)
+        return value
+
+    def get_prep_value(self, value):
+        if value is None:
+            return None
+        elif hasattr(value, "astimezone"):
+            return value.astimezone(pytz.utc)
+        return value
 
 
 class IsAdminRequiredMixin:
