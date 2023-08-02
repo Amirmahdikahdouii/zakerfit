3c5feb52fb9164a9487011bd48b89d7a667dba7b
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2023-08-02 21:21:43 +0330

    make some changes in filtering transactions and move templatetags directory

--- Transactions/views.py
+++ Transactions/views.py
@@ -12,4 +12,14 @@ class UserTransactionsListView(LoginRequiredMixin, ListView):
     context_object_name = "user_transactions"
 
     def get_queryset(self):
-        return self.model.objects.filter(user_id=self.request.user.id).order_by("-id")
+        status_filter = self.request.GET.get("filter_status")
+        payment_filter = self.request.GET.get("filter_payment")
+        if status_filter is None and payment_filter is None:
+            return self.model.objects.filter(user_id=self.request.user.id).order_by("-id")
+        elif payment_filter is None:
+            return self.model.objects.filter(user_id=self.request.user.id, payment_status=status_filter).order_by("-id")
+        elif status_filter is None:
+            return self.model.objects.filter(user_id=self.request.user.id, payment_way=payment_filter).order_by("-id")
+        else:
+            return self.model.objects.filter(user_id=self.request.user.id, payment_way=payment_filter,
+                                             payment_status=status_filter).order_by("-id")
